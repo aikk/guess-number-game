e9f740da6c3c3368cabdfcfba2853b938ac109f4
yerlan <ynurlibayev@gmail.com>
2020-07-13 01:25:37 +0600

    Create new 2 models

--- game/models.py
+++ game/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Game(models.Model):
+    min_number = models.IntegerField()
+    max_number = models.IntegerField()
+    number = models.IntegerField()
+    is_active = models.BooleanField(default=True)
+
+
+class Guess(models.Model):
+    number = models.IntegerField()
+    time = models.DateField(auto_now=False, auto_now_add=True)
+    is_correct = models.BooleanField(default=False)
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
