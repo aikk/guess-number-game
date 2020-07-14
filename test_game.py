991118e265a076ab1cd2091001383dfa708fd654
yerlan <ynurlibayev@gmail.com>
2020-07-14 11:26:51 +0600

    add test case for guessing numbers

--- test_game.py
+++ test_game.py
@@ -12,6 +12,7 @@ def test_index_page(live_server, driver, default_games):
 
 def test_new_game_create(live_server, driver):
     driver.get(live_server.url + '/new_game/')
+
     max_num = driver.find_element_by_css_selector('[data-test="max_number"]')
     max_num.send_keys(10)
     min_num = driver.find_element_by_css_selector('[data-test="min_number"]')
@@ -19,16 +20,49 @@ def test_new_game_create(live_server, driver):
     element = driver.find_element_by_css_selector('[data-test="go_ahead"]')
     element.click()
     game = Game.objects.last()
+
     assert 5 == game.min_number
     assert 10 == game.max_number
-    assert 5 <= game.number < 10
+    assert 5 <= game.number <= 10
     assert str(game.id) in driver.current_url
 
 
-# def test_guess_correct(live_server, driver, default_games):
-#     driver.get(live_server.url + 'game_detail' + str(default_games[0].id))
-#     element = driver.find_element_by_css_selector('[data-test="guess"]')
-#     element.send_keys(7)
-#     element1 = driver.find_element_by_css_selector('[data-test="guess_submit"]')
+def test_guess_correct(live_server, driver, default_games):
+    driver.get(live_server.url + '/' + str(default_games[0].id) + '/')
+    element = driver.find_element_by_css_selector('[data-test="guess_number"]')
+    element.send_keys(7)
+    element1 = driver.find_element_by_css_selector('[data-test="submit"]')
+    element1.click()
+    # assert not default_games[0].is_active
+    is_over = driver.find_element_by_css_selector('[data-test="is_over"]')
+    assert is_over.text == 'The game is over.'
+
+
+def test_guess_incorrect(live_server, driver, default_games):
+    driver.get(live_server.url + '/' + str(default_games[0].id) + '/')
+    element = driver.find_element_by_css_selector('[data-test="guess_number"]')
+    element.send_keys(8)
+    element1 = driver.find_element_by_css_selector('[data-test="submit"]')
+    element1.click()
+    assert default_games[0].is_active
+
+
+# def test_guess_is_not_int(live_server, driver, default_games):
+#     driver.get(live_server.url + '/' + str(default_games[0].id) + '/')
+#     element = driver.find_element_by_css_selector('[data-test="guess_number"]')
+#     element.send_keys('hello')
+#     element1 = driver.find_element_by_css_selector('[data-test="submit"]')
 #     element1.click()
-#     assert not default_games[0].is_active
+#     assert 'error' in driver.current_url
+
+
+# def test_guess_is_out_of_range(live_server, driver, default_games):
+#     driver.get(live_server.url + '/1/')
+
+#     guess_number = driver.find_element_by_css_selector('[data-test="guess_number"]')
+#     button_make_guess = driver.find_element_by_css_selector('[data-test="submit"]')
+
+#     guess_number.send_keys(1000)
+
+#     button_make_guess.click()
+#     assert 'error' in driver.current_url
